ef23923c616013e8fbabc8205a659051b43a710f
Alexey <mskdemin@gmail.com>
2023-06-29 22:10:14 +0300

    11

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+
+// const routes = require('./routes');
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
-app.listen(PORT);
+app.listen(PORT, () => console.log('ok'));
