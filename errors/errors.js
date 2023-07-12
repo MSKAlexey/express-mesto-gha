a1f6c55fda1e855aa7db34e77cb2460e4cebed6f
Alexey Demin <mskdemin@gmail.com>
2023-07-12 14:28:12 +0300

    чет не пашут новые ошибки

--- errors/errors.js
+++ errors/errors.js
@@ -1,5 +1,50 @@
+// eslint-disable-next-line max-classes-per-file
+class CreateAndEditUserCardProfileError extends Error {
+  constructor(err) {
+    super(err);
+    this.message = 'При создании произошла ошибка';
+    this.statusCode = err.statusCode;
+  }
+}
+
+class NotFoundError extends Error {
+  constructor(err) {
+    super(err);
+    this.message = 'Пользователь не найден';
+    this.statusCode = err.statusCode;
+  }
+}
+
+class AuthError extends Error {
+  constructor(err) {
+    super(err);
+    this.message = 'Пользователь не найден';
+    this.statusCode = err.statusCode;
+  }
+}
+
+class GeneralError extends Error {
+  constructor(err) {
+    super(err);
+    this.message = 'Ошибка на стороне сервера';
+    this.statusCode = 500;
+  }
+}
+
 const errorHandler = (err, req, res, next) => {
-  res.status(500).send({ message: 'Произошла ошибка на стороне сервера' });
+  let error;
+
+  if (err.statusCode === 400) {
+    error = new CreateAndEditUserCardProfileError(err);
+  } else if (err.statusCode === 404) {
+    error = new NotFoundError(err);
+  } else if (err.statusCode === 500) {
+    error = new GeneralError(err);
+  } else if (err.statusCode === 403) {
+    error = new AuthError(err);
+  }
+
+  res.status(err.statusCode).send({ message: error.message });
   next();
 };
 
