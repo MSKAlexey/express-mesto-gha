2c2269ec36cafec7de06b60c439f8ad5fc7ae417
Alexey Demin <mskdemin@gmail.com>
2023-07-12 15:25:12 +0300

    111

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
-const errorHandler = require('./errors/errors');
+// const errorHandler = require('./errors/errors');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
@@ -21,6 +21,6 @@ app.use(cookieParser());
 
 app.use(routes);
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
 app.listen(PORT);
--- errors/errors.js
+++ errors/errors.js
@@ -1,50 +1,40 @@
 // eslint-disable-next-line max-classes-per-file
-class CreateAndEditUserCardProfileError extends Error {
-  constructor(err) {
-    super(err);
-    this.message = 'При создании произошла ошибка';
-    this.statusCode = err.statusCode;
-  }
-}
+// class CreateAndEditUserCardProfileError extends Error {
+//   constructor(err) {
+//     super(err);
+//     this.message = 'При создании произошла ошибка';
+//     this.statusCode = err.statusCode;
+//   }
+// }
 
-class NotFoundError extends Error {
-  constructor(err) {
-    super(err);
-    this.message = 'Пользователь не найден';
-    this.statusCode = err.statusCode;
-  }
-}
+// class NotFoundError extends Error {
+//   constructor(err) {
+//     super(err);
+//     this.message = 'Пользователь не найден';
+//     this.statusCode = err.statusCode;
+//   }
+// }
 
-class AuthError extends Error {
-  constructor(err) {
-    super(err);
-    this.message = 'Пользователь не найден';
-    this.statusCode = err.statusCode;
-  }
-}
-
-class GeneralError extends Error {
-  constructor(err) {
-    super(err);
-    this.message = 'Ошибка на стороне сервера';
-    this.statusCode = 500;
-  }
-}
+// class GeneralError extends Error {
+//   constructor(err) {
+//     super(err);
+//     this.message = 'Ошибка на стороне сервера';
+//     this.statusCode = 500;
+//   }
+// }
 
 const errorHandler = (err, req, res, next) => {
-  let error;
+  // let error;
 
-  if (err.statusCode === 400) {
-    error = new CreateAndEditUserCardProfileError(err);
-  } else if (err.statusCode === 404) {
-    error = new NotFoundError(err);
-  } else if (err.statusCode === 500) {
-    error = new GeneralError(err);
-  } else if (err.statusCode === 403) {
-    error = new AuthError(err);
-  }
+  // if (err.statusCode === 400) {
+  //   error = new CreateAndEditUserCardProfileError(err);
+  // } else if (err.statusCode === 404) {
+  //   error = new NotFoundError(err);
+  // } else {
+  //   error = new GeneralError(err);
+  // }
 
-  res.status(err.statusCode).send({ message: error.message });
+  res.status(500).send({ message: 'Ошибка на сервере' });
   next();
 };
 
